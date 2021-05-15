100211a81bd974ee7b8378476ed4fad45f79d251
EC2 Default User <paul.b.friel@me.com>
2021-05-15 16:15:11 +0000

    image loaded function added to support a semaless scroll of images. Image count set to 30 and loader icon now appears upon loading of API data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 
-const count = 10;
+const count = 30;
 const apiKey = 'tV04UgOfs-dy1B6_Drt6E9rwNKWqtDOgbB3eGRF2nYg';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // random & count elements of url address taken from Unsplash > photos > get a random photo 
+
+// Check if all images were loaded. Function is called for each individual image loaded. 
+function imageLoaded() {
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 
 // Helper Function - Refactoring the items previously held in displayPhotos function
 function setAttributes(element, attributes) {
@@ -22,6 +36,8 @@ function setAttributes(element, attributes) {
 // Note - item.setAttribute DOM method has been commented out in favour of the helper function - see above
 
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // Create <a> anchor element to link to UnSplash
@@ -42,6 +58,11 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        
+        // Event Listener, check when each image is finished loading
+        img.addEventListener('load', imageLoaded);
+        
+        
         // Put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -62,9 +83,9 @@ async function getPhotos() {
 
 // Upon scrolling to near bottom of page to trigger load of more photos
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
         getPhotos();
-        console.log('load more');
     }
 });
 
