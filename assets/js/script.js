510409b9354c617684d3f089a19124e880a0279b
EC2 Default User <paul.b.friel@me.com>
2021-05-15 15:09:38 +0000

    displayPhotos functions created to ensure new random image is posted on a new browser window and image attributes for image size and accessibility properties were fetched via the API data source.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
 
 const count = 10;
@@ -6,13 +11,49 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 // random & count elements of url address taken from Unsplash > photos > get a random photo 
 
+// Helper Function - Refactoring the items previously held in displayPhotos function
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+
+function displayPhotos() {
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> anchor element to link to UnSplash
+        const item = document.createElement('a');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+           href: photo.links.html,
+           target: '_blank',
+        });
+        // Create <img> tag for photo
+        const img = document.createElement('img');
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        // Put <img> inside <a>, then put both inside imageContainer element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
 // Get Photos from Unsplash API
 
 async function getPhotos() {
     try {
       const response = await fetch(apiUrl);   
-      const data = await response.json();
-      console.log(data);
+      photosArray = await response.json();
+      displayPhotos();
     } catch (error) {
       // Catch Error     
     }
